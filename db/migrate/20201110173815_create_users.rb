23a6505f0dbe2379982306a351314795dc060f6c
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-11-10 22:35:35 +0200

    Create user model and users controller

---
+++ db/migrate/20201110173815_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.text :email
+      t.text :password
+
+      t.timestamps
+    end
+  end
+end

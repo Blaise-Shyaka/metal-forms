e4154b13b23129b8de452914cbccb6b970b72532
edxco <eduardo.baeza@yahoo.com>
2020-11-11 16:45:22 -0600

    Using #form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
   def new
   end
+
   def create
-    @user = User.new(user_params)
-    
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
-    end
+   @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+   if @user.save
+     redirect_to new_user_path
+   else
+     render :new
+   end
   end
+
+  # def create
+  #   @user = User.new(user_params)
+    
+  #   if @user.save
+  #     redirect_to new_user_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
   private
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 end

35db164e07a8ce61edc095244d694e9c0c48c016
edxco <eduardo.baeza@yahoo.com>
2020-11-11 23:25:42 -0600

    REadme file add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [ :new, :create ] 
+  root to: 'users#new'
+  resources :users, only: %i[new create edit update show destroy]
 end
